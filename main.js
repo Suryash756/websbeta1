711dde7ded6fa58ba44198944500b5ec8a3272bc
Suryash756 <86527278+Suryash756@users.noreply.github.com>
2022-05-17 03:18:08 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,127 @@
+
+function go(){
+    search= document.getElementById("input_search").value;
+    a_href= document.getElementById("search");
+    if(search=="google"){
+     location.href="#section1";
+    }
+    if(search=="GOOGLE"){
+        location.href="#section1";
+       }
+       if(search=="amazon"){
+        location.href="#section1";
+       }
+       if(search=="AMAZON"){
+        location.href="#section1";
+       }
+       if(search==" facebook"){
+        location.href="#section1";
+       }
+       if(search=="FACEBOOK"){
+        location.href="#section1";
+       }
+       if(search=="flipkart"){
+        location.href="#section1";
+       }
+       if(search=="FLIPKART"){
+        location.href="#section1";
+       }
+       if(search=="youtube"){
+        location.href="#section2";
+       }
+       if(search=="YOUTUBE"){
+        location.href="#section2";
+       }
+       if(search=="instagram"){
+        location.href="#section2";
+       }
+       if(search=="INSTAGRAM"){
+        location.href="#section2";
+       }
+       if(search=="twitter"){
+        location.href="#section2";
+       }
+       if(search=="TWITTER"){
+        location.href="#section2";
+       }
+       if(search=="uber"){
+        location.href="#section2";
+       }
+       if(search=="UBER"){
+        location.href="#section2";
+       }
+       if(search=="OYO"){
+        location.href="#section3";
+       }
+       if(search=="oyo"){
+        location.href="#section3";
+       }
+       if(search=="ola"){
+        location.href="#section3";
+       }
+       if(search=="OLA"){
+        location.href="#section3";
+       }
+       if(search=="zomato"){
+        location.href="#section3";
+       }
+       if(search=="ZOMATO"){
+        location.href="#section3";
+       }
+       if(search=="swiggy"){
+        location.href="#section3";
+       }
+       if(search=="SWIGGY"){
+        location.href="#section3";
+       }
+       if(search=="byjus"){
+        location.href="#section4";
+       }
+       if(search=="BYJUS"){
+        location.href="#section4";
+       }
+       if(search=="vedantu"){
+        location.href="#section4";
+       }
+       if(search=="VEDANTU"){
+        location.href="#section4";
+       }
+       if(search=="whitehatjr"){
+        location.href="#section4";
+       }
+       if(search=="whitehat jr"){
+        location.href="#section4";
+       }
+       if(search=="WHITEHAT JR"){
+        location.href="#section4";
+       }
+       if(search=="WHITEHATJR"){
+        location.href="#section4";
+       }
+       if(search=="whitehat Jr"){
+        location.href="#section4";
+       }
+       if(search=="akash"){
+        location.href="#section4";
+       }
+       if(search=="AKASH"){
+        location.href="#section4";
+       }
+       else{
+           alert("opps please re-type😊.")
+       }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
