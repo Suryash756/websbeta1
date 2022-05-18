8167b6a7e39c82d7dddeacf9b255eec1c5ae03bb
Suryash756 <86527278+Suryash756@users.noreply.github.com>
2022-05-18 03:39:53 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,461 +2,265 @@
 function go(){
     search= document.getElementById("input_search").value;
     a_href= document.getElementById("search");
-    if(search=="google"){
-     location.href="#section1";
-    }
-    if(search=="GOOGLE"){
+    if (search.toLowerCase()=="google") {
         location.href="#section1";
-       }
-       if(search=="amazon"){
+    } 
+    if (search.toLowerCase()=="amazon") {
         location.href="#section1";
-       }
-       if(search=="AMAZON"){
+    } 
+    if (search.toLowerCase()=="flipkart") {
         location.href="#section1";
-       }
-       if(search==" facebook"){
+    } 
+    if (search.toLowerCase()=="facebook") {
         location.href="#section1";
-       }
-       if(search=="FACEBOOK"){
-        location.href="#section1";
-       }
-       if(search=="flipkart"){
-        location.href="#section1";
-       }
-       if(search=="FLIPKART"){
-        location.href="#section1";
-       }
-       if(search=="youtube"){
+    } 
+    if (search.toLowerCase()=="youtube") {
         location.href="#section2";
-       }
-       if(search=="YOUTUBE"){
+    } 
+     if (search.toLowerCase()=="instagram") {
         location.href="#section2";
-       }
-       if(search=="instagram"){
+    } 
+    if (search.toLowerCase()=="twitter") {
         location.href="#section2";
-       }
-       if(search=="INSTAGRAM"){
+    } 
+    if (search.toLowerCase()=="uber") {
         location.href="#section2";
-       }
-       if(search=="twitter"){
-        location.href="#section2";
-       }
-       if(search=="TWITTER"){
-        location.href="#section2";
-       }
-       if(search=="uber"){
-        location.href="#section2";
-       }
-       if(search=="UBER"){
-        location.href="#section2";
-       }
-       if(search=="OYO"){
+    } 
+    if (search.toLowerCase()=="oyo") {
         location.href="#section3";
-       }
-       if(search=="oyo"){
+    } 
+    if (search.toLowerCase()=="ola") {
         location.href="#section3";
-       }
-       if(search=="ola"){
+    } 
+    if (search.toLowerCase()=="zomato") {
         location.href="#section3";
-       }
-       if(search=="OLA"){
+    } 
+    if (search.toLowerCase()=="swiggy") {
         location.href="#section3";
-       }
-       if(search=="zomato"){
-        location.href="#section3";
-       }
-       if(search=="ZOMATO"){
-        location.href="#section3";
-       }
-       if(search=="swiggy"){
-        location.href="#section3";
-       }
-       if(search=="SWIGGY"){
-        location.href="#section3";
-       }
-       if(search=="byjus"){
+    } 
+    if (search.toLowerCase()=="byjus") {
         location.href="#section4";
-       }
-       if(search=="BYJUS"){
+    }  
+    if (search.toLowerCase()=="vedantu") {
         location.href="#section4";
-       }
-       if(search=="vedantu"){
+    } 
+    if (search.toLowerCase()=="whitehatjr") {
         location.href="#section4";
-       }
-       if(search=="VEDANTU"){
+    } 
+    if (search.toLowerCase()=="whitehat jr") {
         location.href="#section4";
-       }
-       if(search=="whitehatjr"){
+    } 
+    if (search.toLowerCase()=="whitehat") {
         location.href="#section4";
-       }
-       if(search=="whitehat jr"){
+    } 
+    if (search.toLowerCase()=="akash") {
         location.href="#section4";
-       }
-       if(search=="WHITEHAT JR"){
-        location.href="#section4";
-       }
-       if(search=="WHITEHATJR"){
-        location.href="#section4";
-       }
-       if(search=="whitehat Jr"){
-        location.href="#section4";
-       }
-       if(search=="akash"){
-        location.href="#section4";
-       }
-       if(search=="AKASH"){
-        location.href="#section4";
-       }
-       if(search=="Google"){
-           location.href="#section1"
-       }
-       if(search=="Amazon"){
-        location.href="#section1"
-    }
-    if(search=="Flipkart"){
-        location.href="#section1"
-    }
-    if(search=="Facebook"){
-        location.href="#section1"
-    }
-    if(search=="Youtube"){
-        location.href="#section2"
-    }
-    if(search=="Instagram"){
-        location.href="#section2"
-    }
-    if(search=="Twitter"){
-        location.href="#section2"
-    }
-    if(search=="Uber"){
-        location.href="#section2"
-    }
-    if(search=="Oyo"){
-        location.href="#section3"
-    }
-    if(search=="Oyo"){
-        location.href="#section3"
-    }
-    if(search=="Ola"){
-        location.href="#section3"
-    }
-    if(search=="Zomato"){
-        location.href="#section3"
-    }
-    if(search=="Swiggy"){
-        location.href="#section3"
-    }
-    if(search=="Byjus"){
-        location.href="#section4";
-       }
-       if(search=="Whitehat jr"){
-        location.href="#section4";
-       }
-       if(search=="Vedantu"){
-        location.href="#section4";
-       }
-       if(search=="Akash"){
-        location.href="#section4";
-       }
-       if(search=="suryash"){
-           console.log("suryash jha")
-       }
-       if(search=="wikipedia"){
-           location.href="#section5";
-       }
-       if(search=="WIKIPEDIA"){
+    } 
+    if (search.toLowerCase()=="wikipedia") {
+        location.href="#section5";
+    } 
+    if (search.toLowerCase()=="yahoo") {
+        location.href="#section5";
+    } 
+    if (search.toLowerCase()=="reddit") {
         location.href="#section5";
     }
-    if(search=="Wikipedia"){
+    if (search.toLowerCase()=="redit") {
         location.href="#section5";
-    }
-    if(search=="yahoo"){
+    }  
+    if (search.toLowerCase()=="bing") {
         location.href="#section5";
-    }
-    if(search=="YAHOO"){
-        location.href="#section5";
-    }
-    if(search=="Yahoo"){
-        location.href="#section5";
-    }
-    if(search=="reddit"){
-        location.href="#section5";
-    }
-    if(search=="Reddit"){
-        location.
-        href="#section5";
-    }
-    if(search=="REDDIT"){
-        location.href="#section5";
-    }
-    if(search=="redit"){
-        location.href="#section5";
-    }
-    if(search=="Redit"){
-        location.href="#section5";
-    }
-    if(search=="REDIT"){
-        location.href="#section5";
-    }
-    if(search=="bing"){
-        location.href="#section5";
-    }
-    if(search=="Bing"){
-        location.href="#section5";
-    }
-    if(search=="BING"){
-        location.href="#section5";
-    }
-    if(search=="netflix"){
+    } 
+    if (search.toLowerCase()=="netflix") {
         location.href="#section6";
     }
-    if(search=="Netflix"){
+    if (search.toLowerCase()=="disney") {
         location.href="#section6";
     }
-    if(search=="NETFLIX"){
+    if (search.toLowerCase()=="disney+") {
         location.href="#section6";
     }
-    if(search=="disney plus"){
+    if (search.toLowerCase()=="disneyplus") {
         location.href="#section6";
     }
-    if(search=="Disney plus"){
+    if (search.toLowerCase()=="disney plus") {
         location.href="#section6";
     }
-    if(search=="DISNEY PLUS"){
+    if (search.toLowerCase()=="disneyplushotstar") {
         location.href="#section6";
     }
-    if(search=="disneyplus"){
+    if (search.toLowerCase()=="disney plus hotstar") {
         location.href="#section6";
     }
-    if(search=="Disneyplus"){
+    if (search.toLowerCase()=="hotstar") {
         location.href="#section6";
     }
-    if(search=="DISNEYPLUS"){
+    if (search.toLowerCase()=="amazonprime") {
         location.href="#section6";
     }
-    if(search=="disney +"){
+    if (search.toLowerCase()=="amazon prime") {
         location.href="#section6";
     }
-    if(search=="Disney +"){
+    if (search.toLowerCase()=="prime") {
         location.href="#section6";
     }
-    if(search=="DISNEY +"){
+    if (search.toLowerCase()=="primevideo") {
         location.href="#section6";
     }
-    if(search=="disney+"){
+    if (search.toLowerCase()=="prime video") {
         location.href="#section6";
     }
-    if(search=="Disney+"){
+    if (search.toLowerCase()=="ebay") {
         location.href="#section6";
     }
-    if(search=="DISNEY+"){
-        location.href="#section6";
+    if (search.toLowerCase()=="walmart") {
+        location.href="#section7";
     }
-    if(search=="disneyplushotstar"){
-        location.href="#section6";
+    if (search.toLowerCase()=="wallmart") {
+        location.href="#section7";
     }
-    if(search=="DISNEYPLUSHOTSTAR"){
-        location.href="#section6";
+    if (search.toLowerCase()=="espn") {
+        location.href="#section7";
     }
-    if(search=="Disneyplushotstar"){
-        location.href="#section6";
+    if (search.toLowerCase()=="fandom") {
+        location.href="#section7";
     }
-    if(search=="disney plus hotstar"){
-        location.href="#section6";
+    if (search.toLowerCase()=="cnn") {
+        location.href="#section7";
     }
-    if(search=="DISNEY PLUS HOTSTAR"){
-        location.href="#section6";
+    if (search.toLowerCase()=="foxnews") {
+        location.href="#section8";
     }
-    if(search=="Disney plus hotstar"){
-        location.href="#section6";
+    if (search.toLowerCase()=="fox news") {
+        location.href="#section8";
     }
-    if(search=="hotstar"){
-        location.href="#section6";
+    if (search.toLowerCase()=="microsoft") {
+        location.href="#section8";
     }
-    if(search=="Hotstar"){
-        location.href="#section6";
+    if (search.toLowerCase()=="twitch") {
+        location.href="#section8";
     }
-    if(search=="HOTSTAR"){
-        location.href="#section6";
+    if (search.toLowerCase()=="craigslist") {
+        location.href="#section8";
     }
-    if(search=="amazon prime"){
-        location.href="#section6";
+    if (search.toLowerCase()=="outlook") {
+        location.href="#section9";
     }
-    if(search=="Amazon prime"){
-        location.href="#section6";
+    if (search.toLowerCase()=="outlok") {
+        location.href="#section9";
     }
-    if(search=="AMAZON PRIME"){
-        location.href="#section6";
+    if (search.toLowerCase()=="instructure") {
+        location.href="#section9";
     }
-    if(search=="PRIME"){
-        location.href="#section6";
+    if (search.toLowerCase()=="theweatherchannel") {
+        location.href="#section9";
     }
-    if(search=="prime"){
-        location.href="#section6";
+    if (search.toLowerCase()=="the weather channel") {
+        location.href="#section9";
     }
-    if(search=="Prime"){
-        location.href="#section6";
+    if (search.toLowerCase()=="office") {
+        location.href="#section9";
     }
-    if(search=="prime video"){
-        location.href="#section6";
+    if (search.toLowerCase()=="microsoftoffice") {
+        location.href="#section9";
     }
-    if(search=="Prime video"){
-        location.href="#section6";
+    if (search.toLowerCase()=="microsoft office") {
+        location.href="#section9";
     }
-    if(search=="PRIME VIDEO"){
-        location.href="#section6";
+    if (search.toLowerCase()=="accuweather") {
+        location.href="#section10";
     }
-    if(search=="ebay"){
-        location.href="#section6";
+    if (search.toLowerCase()=="accu weather") {
+        location.href="#section10";
     }
-    if(search=="Ebay"){
-        location.href="#section6";
+    if (search.toLowerCase()=="zillow") {
+        location.href="#section10";
     }
-    if(search=="EBAY"){
-        location.href="#section6";
+    if (search.toLowerCase()=="zilow") {
+        location.href="#section10";
     }
-    if(search=="walmart"){
-        location.href="#section7"
+    if (search.toLowerCase()=="zillo") {
+        location.href="#section10";
     }
-    if(search=="Walmart"){
-        location.href="#section7"
+    if (search.toLowerCase()=="zilo") {
+        location.href="#section10";
     }
-    if(search=="espn"){
-        location.href="#section7"
+    if (search.toLowerCase()=="imdb") {
+        location.href="#section10";
     }
-    if(search=="Espn"){
-        location.href="#section7"
+    if (search.toLowerCase()=="u.s.p.s") {
+        location.href="#section10";
     }
-    if(search=="ESPN"){
-        location.href="#section7"
+    if (search.toLowerCase()=="usps") {
+        location.href="#section10";
     }
-    if(search=="fandom"){
-        location.href="#section7"
+    if (search.toLowerCase()=="unitedstatespostalservice") {
+        location.href="#section10";
     }
-    if(search=="Fandom"){
-        location.href="#section7"
+    if (search.toLowerCase()=="unitedstates postal service") {
+        location.href="#section10";
     }
-    if(search=="FANDOM"){
-        location.href="#section7"
+    if (search.toLowerCase()=="united states postal service") {
+        location.href="#section10";
     }
-    if(search=="cnn"){
-        location.href="#section7"
+    if (search.toLowerCase()=="pintrest") {
+        location.href="#section11";
     }
-    if(search=="Cnn"){
-        location.href="#section7"
+    if (search.toLowerCase()=="target") {
+        location.href="#section11";
     }
-    if(search=="CNN"){
-        location.href="#section7"
+    if (search.toLowerCase()=="quizelt") {
+        location.href="#section11";
     }
-    if(search=="foxnews"){
-        location.href="#section8"
+    if (search.toLowerCase()=="linkedin") {
+        location.href="#section11";
     }
-    if(search=="Foxnews"){
-        location.href="#section8"
+    if (search.toLowerCase()=="linked in") {
+        location.href="#section11";
     }
-    if(search=="FOXNEWS"){
-        location.href="#section8"
+    if (search.toLowerCase()=="thenewyorktimes") {
+        location.href="#section12";
     }
-    if(search=="fox news"){
-        location.href="#section8"
+    if (search.toLowerCase()=="the newyork times") {
+        location.href="#section12";
     }
-    if(search=="Fox news"){
-        location.href="#section8"
+    if (search.toLowerCase()=="nytimes") {
+        location.href="#section12";
     }
-    if(search=="FOX NEWS"){
-        location.href="#section8"
+    if (search.toLowerCase()=="duckduckgo") {
+        location.href="#section12";
     }
-    if(search=="microsoft"){
-        location.href="#section8"
+    if (search.toLowerCase()=="duck duck go") {
+        location.href="#section12";
     }
-    if(search=="Microsoft"){
-        location.href="#section8"
+    if (search.toLowerCase()=="msn") {
+        location.href="#section12";
     }
-    if(search=="MICROSOFT"){
-        location.href="#section8"
+    if (search.toLowerCase()=="zoom") {
+        location.href="#section12";
     }
-    if(search=="twitch"){
-        location.href="#section8"
+    if (search.toLowerCase()=="outbrain") {
+        location.href="#section13";
     }
-    
-    if(search=="Twitch"){
-        location.href="#section8"
+    if (search.toLowerCase()=="out brain") {
+        location.href="#section13";
     }
-    
-    if(search=="TWITCH"){
-        location.href="#section8"
+    if (search.toLowerCase()=="healthline") {
+        location.href="#section13";
     }
-    
-    if(search=="craigslist"){
-        location.href="#section8"
+    if (search.toLowerCase()=="xfinity") {
+        location.href="#section13";
     }
-    if(search=="Craigslist"){
-        location.href="#section8"
+    if (search.toLowerCase()=="x finity") {
+        location.href="#section13";
     }
-    if(search=="CRAIGSLIST"){
-        location.href="#section8"
+    if (search.toLowerCase()=="tata") {
+        location.href="#section14";
     }
-    if(search=="outlook"){
-        location.href="#section9"
+    if(search.toLowerCase()=="reliance"){
+        location.href="#section13"
     }
-    
-    if(search=="Outlook"){
-        location.href="#section9"
+    if(search.toLowerCase()=="relaince"){
+        location.href="#section13"
     }
-    
-    if(search=="OUTLOOK"){
-        location.href="#section9"
-    }
-    
-    if(search=="instructure"){
-        location.href="#section9"
-    }
-    
-    if(search=="Instructure"){
-        location.href="#section9"
-    }
-    
-    if(search=="INSTRUCTURE"){
-        location.href="#section9"
-    }
-    
-    if(search=="the weather channel"){
-        location.href="#section9"
-    }
-    
-    if(search=="The weather channel"){
-        location.href="#section9"
-    }
-    
-    if(search=="THE WEATHER CHANNEL"){
-        location.href="#section9"
-    }
-    if(search=="microsoft office"){
-        location.href="#section9"
-    }
-    if(search=="Microsoft office"){
-        location.href="#section9"
-    }
-    if(search=="MICROSOFT OFFICE"){
-        location.href="#section9"
-    }
-    if(search=="microsoftoffice"){
-        location.href="#section9"
-    }
-    if(search=="Microsoftoffice"){
-        location.href="#section9"
-    }
-    if(search=="MICROSOFTOFFICE"){
-        location.href="#section9"
-    }
-    if(search=="office"){
-        location.href="#section9"
-    }
-    if(search=="Office"){
-        location.href="#section9"
-    }
-    if(search=="OFFICE"){
-        location.href="#section9"
-    }
-
 }
